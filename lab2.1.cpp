be2d12bb4d914cd6fa088090234d775925db6493
YanaKkr <yana.kukhar.ik.2023@lpnu.ua>
2023-10-03 11:52:32 +0300

    розкоментувала z2

--- lab2.1.cpp
+++ lab2.1.cpp
@@ -12,17 +12,17 @@ int main()
 {
     double a; // вхідний параметр
     double z1; // результат 1 виразу
-    // double z2; // результат 2 виразу
+    double z2; // результат 2 виразу
 
     cout << "a = "; cin >> a;
 
     z1 = ( (1-2*sin(a)*sin(a)))/(1+2*sin(a)*cos(a) );
 
-    // z2 = ( 1-tan(a))/(1+tan(a) );
+    z2 = ( 1-tan(a))/(1+tan(a) );
 
     cout << endl;
     cout << "z1 = " << z1 << endl;
-   // cout << "z2 = " << z2 << endl;
+    cout << "z2 = " << z2 << endl;
 
     cin.get();
     return 0;
